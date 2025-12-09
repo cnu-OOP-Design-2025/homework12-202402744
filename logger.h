011ec068a32014634b48d21d0beffb0a5c00e86d
202402744 <kkultteog11@gmail.com>
2025-12-09 13:42:33 +0900

    과제 제출

--- logger.h
+++ logger.h
@@ -1,9 +1,11 @@
-
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <mutex>
 #include <thread>
 #include <string>
+#include <memory>
+#include <stdexcept>
 
 class Logger {
 private:
@@ -12,14 +14,43 @@ private:
     static std::mutex write_mtx;
     std::ofstream logFile;
 
+    Logger(const std::string& filename) {
+        logFile.open(filename, std::ios::trunc);
+        if (!logFile.is_open()) {
+            throw std::runtime_error("Failed to open log file: " + filename);
+        }
+        log("[Init] Logger started.");
+    }
+
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    Logger(Logger&&) = delete;
+    Logger& operator=(Logger&&) = delete;
+
 
 public:
+    ~Logger() {
+        if (logFile.is_open()) {
+            log("[Shutdown] Logger closed.");
+            logFile.close();
+        }
+    }
+
     static Logger* getInstance(const std::string& filename = "Test/output2.txt") {
-        return nullptr;
+        if (!instance) {
+            std::lock_guard<std::mutex> lock(init_mtx);
+            if (!instance) {
+                instance = std::unique_ptr<Logger>(new Logger(filename));
+            }
+        }
+        return instance.get();
     }
 
     void log(const std::string& message) {
-        /* TODO */
+        std::lock_guard<std::mutex> lock(write_mtx);
+        if (logFile.is_open()) {
+            logFile << message << std::endl;
+        }
     }
 
 };
@@ -27,4 +58,3 @@ public:
 std::unique_ptr<Logger> Logger::instance;
 std::mutex Logger::init_mtx;
 std::mutex Logger::write_mtx;
-
--- main.cpp
+++ main.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <stdexcept> 
 #include "character.h"
-
+#include "logger.h"
 using namespace std;
 
 void equip_test1(shared_ptr<Character> c1) {
-    c1 = make_shared<Armor>(c1);
-    c1 = make_shared<Boots>(c1);
-    c1 = make_shared<Sword>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+    try {
+        shared_ptr<Character> decorated = c1;
+        decorated = make_shared<Armor>(decorated);
+        decorated = make_shared<Boots>(decorated);
+        decorated = make_shared<Sword>(decorated);
+        cout << decorated->getDescription()
+            << " | Attack: " << decorated->getAttack()
+            << " | Speed: " << decorated->getSpeed()
+            << " | Defense: " << decorated->getDefense() << endl;
+    } catch (const invalid_argument& e) {
+        
+        cout << "Invalid equip: " << e.what() << endl;
+       
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
 void equip_test2(shared_ptr<Character> c1) {
-    c1 = make_shared<Armor>(c1);
-    c1 = make_shared<Bow>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+    try {
+        shared_ptr<Character> decorated = c1; 
+        decorated = make_shared<Armor>(decorated);
+        decorated = make_shared<Bow>(decorated);
+        cout << decorated->getDescription()
+            << " | Attack: " << decorated->getAttack()
+            << " | Speed: " << decorated->getSpeed()
+            << " | Defense: " << decorated->getDefense() << endl;
+    } catch (const invalid_argument& e) {
+       
+        cout << "Invalid equip: " << e.what() << endl;
+       
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
 void equip_test3(shared_ptr<Character> c1) {
-    c1 = make_shared<Boots>(c1);
-    c1 = make_shared<Staff>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+    try {
+        shared_ptr<Character> decorated = c1; 
+        decorated = make_shared<Boots>(decorated);
+        decorated = make_shared<Staff>(decorated);
+        cout << decorated->getDescription()
+            << " | Attack: " << decorated->getAttack()
+            << " | Speed: " << decorated->getSpeed()
+            << " | Defense: " << decorated->getDefense() << endl;
+    } catch (const invalid_argument& e) {
+        
+        cout << "Invalid equip: " << e.what() << endl;
+        
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
 void doTest() {
@@ -41,7 +66,7 @@ void doTest() {
 
     for(auto player: players){
         cout << "-------------------------------------------------------------" << endl;
-        equip_test1(player);
+        equip_test1(player); 
         equip_test2(player);
         equip_test3(player);
     }
@@ -55,7 +80,6 @@ struct Student{
     Student() = delete;
     ~Student() { cout << "Delete Student " << name << endl; }
 };
-
 
 
 int main(int argc, const char *argv[]) {
